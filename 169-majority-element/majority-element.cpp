c66e5e9825caa68be2657429f371ed99713bc38e
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2024-03-20 19:57:29 -0400

    Time: 19 ms (24.49%) | Memory: 28 MB (6.81%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> a;
+        for(int i:nums)
+            a[i]++;
+        int maxval=nums[0];
+        for(auto x:a)
+        {
+            if(x.second>a[maxval])
+                maxval=x.first;
+        }
+        return maxval;
+    }
+};
