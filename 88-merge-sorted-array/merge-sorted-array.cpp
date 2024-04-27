7a9deaa88f98302fa2c5dcac76bf94c9d512b978
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2024-04-26 20:15:12 -0400

    Time: 0 ms (100.00%) | Memory: 11 MB (31.22%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-        
-        while (j >= 0) {
-            if (i >= 0 && nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
-                k--;
+        // [1,2,3,12,0,0,0,0] m=4 a[x]
+        // [8,10,12,14] n=2 b[x]
+  		//first pass[1,2,3,12,0,0,0,14]
+  		//second pass[1,2,3,12,0,0,12,14]
+  		//third pass [1,2,3,12,0,12,12,14]
+  		//fourth pass [1,2,3,12,10,12,12,14]
+ 		//fifth pass [1,2,3,8,10,12,12,14]
+        int i = m-1;
+        int k = n-1;
+        int j= m+n-1;
+
+        while(k>=0){
+            if(i>=0&&nums1[i]>=nums2[k]){//need to make sure i>0
+                nums1[j]=nums1[i];
                 i--;
-            } else {
-                nums1[k] = nums2[j];
+                j--;
+            }else{
+                nums1[j]=nums2[k];
                 k--;
                 j--;
             }
         }
     }
-    };
+};
