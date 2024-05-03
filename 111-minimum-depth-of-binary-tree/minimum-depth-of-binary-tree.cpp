370230546da79163356e0d9e09961a0390003e5d
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2024-05-02 23:09:46 -0400

    Time: 173 ms (89.35%) | Memory: 144.7 MB (83.81%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -11,16 +11,15 @@
  */
 class Solution {
 public:
+    int minDepthRecv(TreeNode* root) {
+        if (!root) return INT_MAX;
+        if (!root->left && !root->right) return 1;
+        return 1 + min(minDepthRecv(root->left), minDepthRecv(root->right)
+        );
+    }
+
     int minDepth(TreeNode* root) {
-        if(root==NULL) return 0;
-        if(root->left==NULL && root->right==NULL) {
-            return 1;
-        }
-        int l=INT_MAX,r=INT_MAX;
-        if(root->left)
-            l=minDepth(root->left);
-        if(root->right)
-            r=minDepth(root->right);
-        return 1+(min(l,r));
+        if (!root) return 0;
+        return minDepthRecv(root);
     }
 };
