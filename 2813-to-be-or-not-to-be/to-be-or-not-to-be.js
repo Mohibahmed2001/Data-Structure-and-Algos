70bc71d49bda8e00aa5d63d4d49743422e70fb11
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2024-06-27 23:17:55 -0400

    Time: 60 ms (14.97%) | Memory: 49.1 MB (12.91%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -2,18 +2,29 @@
  * @param {string} val
  * @return {Object}
  */
-var expect = function(val) {
-  return {
-    toBe: (val2) => {
-      if(val !== val2) throw new Error("Not Equal");
-      return true;
-    },
-    notToBe: (val2) => {
-      if(val === val2) throw new Error("Equal");
-      return true;
+class Expect {
+    constructor(val) {
+        this.val = val;
     }
-  };
-};
+    
+    toBe(val2) {
+        if (this.val !== val2) {
+            throw new Error("Not Equal");
+        }
+        return true;
+    }
+    
+    notToBe(val2) {
+        if (this.val === val2) {
+            throw new Error("Equal");
+        }
+        return true;
+    }
+}
+
+function expect(val) {
+    return new Expect(val);
+}
 
 /**
  * expect(5).toBe(5); // true
