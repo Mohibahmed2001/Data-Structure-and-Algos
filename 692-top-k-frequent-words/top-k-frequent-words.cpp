0475d4814edf2657bdc9d6ca5b431e8c49347c4d
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2024-06-29 23:46:42 -0400

    Time: 8 ms (81.55%) | Memory: 16.4 MB (78.39%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        map<string,int> mp;
+        vector<string> ans;
+        
+        // Count frequencies of each word
+        for (string element : words) {
+            mp[element]++;
+        }
+        
+        // Create a vector of pairs (frequency, word)
+        vector<pair<int,string>> v;
+        for (auto it : mp) {
+            v.push_back(make_pair(it.second, it.first));
+        }
+        
+        // Sort primarily by frequency (in descending order)
+        // If frequencies are the same, then sort alphabetically (in ascending order)
+        sort(v.begin(), v.end(), [](const pair<int,string>& a, const pair<int,string>& b) {
+            if (a.first == b.first) {
+                return a.second < b.second; // Alphabetical order
+            } else {
+                return a.first > b.first; // Decreasing order of frequency
+            }
+        });
+        
+        // Collect the top k frequent words
+        for (int i = 0; i < k && i < v.size(); ++i) {
+            ans.push_back(v[i].second);
+        }
+        
+        return ans;
+    }
+};
