19e3a856aac19ed5fa679be4703f92e7bfc0523d
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2024-06-30 00:02:51 -0400

    Time: 19 ms (9.77%) | Memory: 16.3 MB (91.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -10,8 +10,8 @@ public:
             mp[nums[i]]++;
         vector<pair<int,int>>v;
         for(auto it : mp )
-            v.push_back(make_pair(it.second,it.first));
-        sort(v.rbegin(),v.rend());
+            v.push_back({-it.second,it.first});
+        sort(v.begin(),v.end());
         for(int i=0;i<v.size() && k!=0 ;i++)
         {
             ans.push_back(v[i].second);
