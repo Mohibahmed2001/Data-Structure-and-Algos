90b70426aa58a425cd88d864743774615a32616f
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2024-08-28 18:51:24 -0400

    Time: 12 ms (62.06%) | Memory: 17.7 MB (18.62%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,18 +1,16 @@
-
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) 
     {
         vector<int>ans;
         map<int,int>mp;
-        //add values to map
         for(int i=0;i<nums.size();i++)
             mp[nums[i]]++;
         vector<pair<int,int>>v;
         for(auto it : mp )
-            v.push_back({it.second,it.first});
-        sort(v.begin(),v.end());
-        for(int i=v.size()-1;i>=0 && k!=0 ;i--)
+            v.push_back(make_pair(it.second,it.first));
+        sort(v.rbegin(),v.rend());
+        for(int i=0;i<v.size() && k!=0 ;i++)
         {
             ans.push_back(v[i].second);
             k--;
