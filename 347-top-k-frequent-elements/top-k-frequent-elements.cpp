5f506ff4988aeea43495033a414c38790af899ba
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2024-09-05 15:02:56 -0400

    Time: 11 ms (68.65%) | Memory: 17.5 MB (54.72%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) 
     {
-        vector<int>ans;
-        map<int,int>mp;
-        for(int i=0;i<nums.size();i++)
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
-        vector<pair<int,int>>v;
-        for(auto it : mp )
-            v.push_back(make_pair(it.second,it.first));
-        sort(v.begin(),v.end());
-        for(int i=v.size()-1;i>=0 && k!=0 ;i--)
-        {
-            ans.push_back(v[i].second);
+        }
+        vector<pair<int,int>>winter;
+        for(auto it:mp){
+            winter.push_back(make_pair(it.second,it.first));
+        }
+        sort(winter.begin(),winter.end());
+        vector<int>ans;
+        for(int i=winter.size()-1;i>=0&&k!=0;i--){
+            ans.push_back(winter[i].second);
             k--;
-        }       
-        return ans; 
+        }
+        return ans;
     }
 };
