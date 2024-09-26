05c05b285785dc61b7e805a481ababbe3a385b64
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2024-09-26 00:18:47 -0400

    Time: 6 ms (95.31%) | Memory: 17.5 MB (57.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) 
-    {
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        vector<pair<int,int>>winter;
-        for(auto it:mp){
-            winter.push_back(make_pair(it.second,it.first));
-        }
-        sort(winter.begin(),winter.end());
-        vector<int>ans;
-        for(int i=winter.size()-1;i>=0&&k!=0;i--){
-            ans.push_back(winter[i].second);
-            k--;
-        }
-        return ans;
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+    //first we put all the elements into a hash map
+    unordered_map<int,int>mp;
+    for(auto elm:nums){
+        mp[elm]++;
+    }  
+    //we not have frequency of every element inside mp
+    priority_queue<pair<int,int>>tp;
+    for(auto elm:mp){
+       tp.push({elm.second,elm.first});
+    }
+    vector<int>answer;
+    while(k>0&&!tp.empty()){
+        answer.push_back(tp.top().second);
+        tp.pop();
+        k--;
+    }
+    return answer;
     }
 };
