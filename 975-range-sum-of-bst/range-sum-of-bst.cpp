0156a68104239f647ef0098e167c95561a078ff3
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2024-09-30 20:06:27 -0400

    Time: 90 ms (55.99%) | Memory: 63.2 MB (68.84%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -12,15 +12,15 @@
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        int total = 0;
-        if (!root) {
+         int total =0;
+        if(!root){
             return 0;
-        } else if (low <= root->val && root->val <= high) {
-            return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
-        } else if (root->val < low) {
-            return rangeSumBST(root->right, low, high);  // Only search the right subtree
-        } else {
-            return rangeSumBST(root->left, low, high);  // Only search the left subtree
+        }else if(low<=root->val && root->val<=high){
+            return root->val + rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high);
+        }else if(root->val<low){
+            return rangeSumBST(root->right,low,high);
+        }else{
+            return rangeSumBST(root->left,low,high);
         }
     }
 };
