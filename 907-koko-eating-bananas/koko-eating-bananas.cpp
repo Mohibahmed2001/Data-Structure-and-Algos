14dc68da9641bc5225235b1a532872ed6de0445a
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2025-06-12 18:23:10 -0400

    Time: 11 ms (43.48%) | Memory: 22.8 MB (96.70%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        // The slowest speed is 1 banana per hour.
-        // The fastest speed needed is the size of the largest pile.
-        int left = 1;
-        int right = *max_element(piles.begin(), piles.end());
+        int i = 1;
+        int j = *max_element(piles.begin(), piles.end());
+        while(i<j){
+            int mid = (i+j)/2;
+            int count = 0;
+            for(int i =0;i<piles.size();i++){
+                count += (piles[i] + mid - 1) / mid;
 
-        // Binary search for the minimum speed that allows Koko to finish within h hours.
-        while (left < right) {
-            int mid = left + (right - left) / 2;  // Avoid potential overflow.
-            if (canFinish(piles, mid, h)) {
-                // If Koko can finish with speed 'mid', try to see if a lower speed might work.
-                right = mid;
-            } else {
-                // Otherwise, she needs to eat faster.
-                left = mid + 1;
+
+            }
+            if(count>h){
+                i = mid+1;
+            }else{
+                j = mid;
             }
         }
-        return left;
-    }
-
-    // Helper function to check if Koko can finish all banana piles at a given speed within h hours.
-    bool canFinish(const vector<int>& piles, int speed, int h) {
-        int totalHours = 0;
-        for (int pile : piles) {
-            // Calculate the hours needed for this pile using ceiling division.
-            // This is equivalent to: ceil(pile / speed)
-            int hours = pile / speed;
-            if (pile % speed != 0) {
-                hours++; // Add an extra hour if there is a remainder.
-            }
-            totalHours += hours;
-        }
-        // Return true if the total time is within the allowed hours.
-        return totalHours <= h;
+        return i;
+        
     }
 };
