6032edc10f516755a8d59d3ada8bf6eb9e5537f5
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2025-06-18 16:10:29 -0400

    Time: 0 ms (100.00%) | Memory: 13.2 MB (75.98%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int low_col = 0;
-        int high_col = matrix.size()-1;
-        int col =-1;
-        while(low_col<=high_col){
-            int mid = (low_col+high_col)/2;
+        int low_row = 0;
+        int high_row = matrix.size()-1;
+        int row = -1;
+        while(low_row<=high_row){
+            int mid = (low_row+high_row)/2;
             if(matrix[mid][0]==target){
                 return true;
             }else if(matrix[mid][0]<target){
-                col = mid;
-                low_col = mid +1;
-                
-            }else{
-                high_col = mid-1;
-            }
-        }
-        if(col ==-1){
-            return false;
-        }
-        int low_row =0;
-        int high_row = matrix[0].size()-1;
-        while(low_row<=high_row){
-            int mid = (low_row+high_row)/2;
-            if(matrix[col][mid]==target){
-                return true;
-            }else if(matrix[col][mid]<target){
-                low_row=mid+1;
+                row = mid;
+                low_row = mid+1;
             }else{
                 high_row = mid-1;
             }
         }
-        return false;;
+        if(row==-1){
+            return false;
+        }
+        int low_col = 0;
+        int high_col = matrix[0].size()-1;
+        while(low_col<=high_col){
+            int mid = (low_col+high_col)/2;
+            if(matrix[row][mid]==target){
+                return true;
+            }else if(matrix[row][mid]<target){
+                low_col = mid+1;
+            }else{
+                high_col = mid-1;
+            }
+        }
+        return false;
+
         
     }
 };
